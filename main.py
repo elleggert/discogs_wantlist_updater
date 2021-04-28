6d9348fa6f6c7c1e539feaa32be665327ae753af
ele20 <ele20@ic.ac.uk>
2021-04-28 21:29:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import discogs_client
+
+#Will need to pass the user-specific token to the app to function accordingly
+#Token can be generated under https://www.discogs.com/settings/developers
+
+
+
+
+print("Please enter your Discogs User-Token:")
+userToken = input()
+
