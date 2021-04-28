b17c973ba3c6acebf769fc6d39f1a947ab27be47
ele20 <ele20@ic.ac.uk>
2021-04-28 21:41:54 +0100

    Initial querying of the discogs API for an example track possible
    Simple identity querying possible

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import discogs_client
 # Token can be generated under https://www.discogs.com/settings/developers
 
 
+
+
 print("Please enter your Discogs User-Token:")
 userToken = input()
 
+# Establishing an API connection and enabling API limiting
 client = discogs_client.Client('WantlistEditor', user_token=userToken)
+client.backoff_enabled = True
+
+# Querying some example releases
 results = client.search('Phylyps Trak II', type='release')
 print(results.pages)
 artist = results[0].artists[0]
 print(artist.name)
+
+me = client.identity()
+print(me.name, me.username, me.location)
+print(len(me.wantlist))
+print(me.wantlist)
 
