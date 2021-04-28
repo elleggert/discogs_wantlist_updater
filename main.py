da6b6d365cd630af23a1694ac0178136bd198032
ele20 <ele20@ic.ac.uk>
2021-04-28 21:36:29 +0100

    Initial querying of the discogs API for an example track possible

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import discogs_client
 
-#Will need to pass the user-specific token to the app to function accordingly
-#Token can be generated under https://www.discogs.com/settings/developers
-
-
+# Will need to pass the user-specific token to the app to function accordingly
+# Token can be generated under https://www.discogs.com/settings/developers
 
 
 print("Please enter your Discogs User-Token:")
 userToken = input()
+
+client = discogs_client.Client('WantlistEditor', user_token=userToken)
+results = client.search('Phylyps Trak II', type='release')
+print(results.pages)
+artist = results[0].artists[0]
+print(artist.name)
 
